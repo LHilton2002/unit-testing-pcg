55b616a8d2139385586db830037b9bb3d366b08c
LHilton2002 <72805699+LHilton2002@users.noreply.github.com>
2022-10-05 11:45:54 +0100

    unit test triangle test
    ~+

--- Assets/Scripts/Runtime/Nodes/Geometry/TriangleNode.cs
+++ Assets/Scripts/Runtime/Nodes/Geometry/TriangleNode.cs
@@ -36,9 +36,30 @@ namespace MiniDini.Nodes
 
             m_geometry.Empty();
 
+            Point a = new();
+            a.position = editplane.up * radius;
+            Point b = new();
+            b.position = Quaternion.AngleAxis(360.0f / 3.0f, editplane.normal) * (editplane.up * radius);
+            Point c = new();
+            c.position = Quaternion.AngleAxis(-360.0f / 3.0f, editplane.normal) * (editplane.up * radius);
+
+            int x = m_geometry.AddPoint(a);
+            int y = m_geometry.AddPoint(b);
+            int z = m_geometry.AddPoint(c);
+
+            Prim p = new();
+
+            p.points.Add(x);
+            p.points.Add(y);
+            p.points.Add(z);
+
+            m_geometry.AddPrim(p);
+
+
+
             // here is where we construct the geometry for a triangle (3 points, one primitive with three indices)
             // try constructing otherwise and see if the unit tests capture the failure!
-            
+
             return m_geometry;
         }
 
